a1859ac2f59034759796402b2be5a35eebcdae96
Jason Shi <jasonshi666@icloud.com>
2020-06-28 04:38:42 -0700

    Initial commit 3: given_facts.hpp basic undebugged arrpgant implementation.

--- src/given_facts.hpp
+++ src/given_facts.hpp
@@ -1,9 +1,76 @@
 #include "headers.hpp"
+#include "./definition/mass.hpp"
 
 /*
 
 CREATE AN INTERACTIVE USER EXPERIENCE BY Q/A
 
 */
+
+int get_string(char *str);
+int get_double(double &d);
+int get_int(int &i);
+int get_char(char &c);
+
+void qa_exec() {
+
+//	char *str;
+//	get_string(str, "Enter a string: ");
+	Mass mass_of_object;
+	double _mass = 0;
+
+	get_double(&_mass, "Enter the mass: ");
+	mass_of_object.mass = _mass;
+
+}
+
+int get_string(char *&str, std::string enquiry) {
+	
+	std::string buffer;
+
+	std::cout << enquiry << std::endl;
+	std::cin >> buffer;
+
+	str = buffer.c_str();
+
+	return 0;
+}
+
+int get_double(double &d, std::string enquiry) {
+	
+	std::string buffer;
+
+	std::cout << enquiry << std::endl;
+	std::cin >> buffer;
+
+	d = strtod(buffer);
+	return 0;
+
+}
+
+int get_int(int &i, std::string enquiry) {
+	
+	std::string buffer;
+
+	std::cout << enquiry << std::Endl;
+	std::cin >> buffer;
+
+	i = atoi(buffer.c_str());
+
+	return 0;
+
+}
+
+int get_char(char &c, std::string enquiry) {
+
+	std::string buffer;
+
+	std::cout << enquiry << std::endl;
+	std::cin >> buffer;
+	
+	c = buffer.c_str()[0];
+	
+	return 0;
+}
 
 

369298e51c3108bd734279e1ba9e1337e8951f08
Jason Shi <jasonshi666@icloud.com>
2020-06-28 04:56:48 -0700

    Initial commit 4: Continue implementing and refining given...

--- src/definition/velocity.hpp
+++ src/definition/velocity.hpp
@@ -13,6 +13,11 @@ public:
        this.direction = displacement.direction;
    }
    
+   Velocity(const double speed) {
+		this.magnitude = speed;
+		this.direction = POSITIVE;
+   }
+   
    double velocity_representation() {
         return (this.direction ? magnitude : -(magnitude));
    }

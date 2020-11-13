982b18d2f02dfd38d2b9821a6b14994ebd5a58d9
Jason Shi <jasonshi666@icloud.com>
2020-11-13 04:50:29 -0800

    Fix headers.hpp to use system generic header file

--- src/headers.hpp
+++ src/headers.hpp
@@ -1,12 +1,14 @@
-#include <math.h>
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <stdbool.h>
-#include <vector>
-#include <map>
-#include <string>
-#include <iomanip>
+//#include <math.h>
+//#include <cstdlib>
+//#include <iostream>
+//#include <fstream>
+//#include <stdbool.h>
+//#include <vector>
+//#include <map>
+//#include <string>
+//#include <iomanip>
+
+#include <bits/stdc++.h>
 
 #define FILE_EXTENSION ".kobj"
 
